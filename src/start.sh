d93f5fc84f8ab1006c8468753e8590a5f2795713
myrtletree33 <me@joeltong.org>
2015-11-04 01:47:55 +0800

    added start script

---
+++ src/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+## Starts Picam daemon in background, if not already running
+./picam &
+
+## Delete all files older than 10 days.  Scan for these files every 60 seconds.
+## CAUTION: This command is very dangerous, ensure is already in correct folder.
+(cd rec && while true; do sleep 2; find . -mtime +10 -delete; done &)
+
+## Start taking photos
+python main.py
